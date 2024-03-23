bb8a63e9224bb94f5fca87d29a3dd92f93a56b92
Marco <kojunjunla@gmail.com>
2024-03-23 23:08:44 +0800

    command in simpleLCS.js

--- simpleLCS.js
+++ simpleLCS.js
@@ -3,6 +3,7 @@ function simpleLCS(str1, str2) {
     return 0;
   } else {
     if (str1[str1.length - 1] == str2[str2.length - 1]) {
+      //substring(字串的起始位置 ,字串的擷取終點位置(包含))
       return (
         1 +
         simpleLCS(
@@ -12,6 +13,7 @@ function simpleLCS(str1, str2) {
       );
     } else {
       return Math.max(
+        //substring(字串的起始位置 ,字串的擷取終點位置(包含))
         simpleLCS(str1.substring(0, str1.length - 1), str2),
         simpleLCS(str1, str2.substring(0, str2.length - 1))
       );
