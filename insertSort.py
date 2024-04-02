264ddd6e0f6ba54e9a4e611103139c076500d7f3
Marco <kojunjunla@okbank.com>
2024-04-02 12:52:41 +0800

    selectsort.py 1st commit

---
+++ insertSort.py
@@ -0,0 +1,9 @@
+arr = [15,669,45,51,848,6,21,48,954,877,32,54,65,3345,31,248,35,8,9999]
+
+def insertSort(arr):
+    for i in range(1,len(arr),1):
+        while (i > 0 and arr[i] < arr[i-1]):
+            arr[i],arr[i-1] = arr[i-1], arr[i]
+            i -= 1
+insertSort(arr)
+print(arr)
