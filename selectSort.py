3b59c869a24144616be0d74c48eca987c7669205
Marco <kojunjunla@okbank.com>
2024-04-02 12:44:43 +0800

    selectSort.py 1st commit

---
+++ selectSort.py
@@ -0,0 +1,14 @@
+arr = [15,669,45,51,848,6,21,48,954,877,32,54,65,3345,31,248,35,8,9999]
+
+def selectSort(arr:list)->list:
+    for i in range(0,len(arr)-1,1):
+        min = float('inf')
+        idx = None
+        for j in range(i,len(arr),1):
+            if arr[j] < min:
+                min = arr[j]
+                idx = j
+        arr[i],arr[idx] = arr[idx],arr[i]
+
+selectSort(arr)
+print(arr)
