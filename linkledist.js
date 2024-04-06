4d67033f7c25be76403a0e3986d7631b5f692b51
Marco <kojunjunla@gmail.com>
2024-04-06 23:25:00 +0800

    add some comand

--- linkledist.js
+++ linkledist.js
@@ -35,16 +35,18 @@ class LinkedList {
       return temp;
     } else {
       let currentNode = this.head;
+      //因為最後一個(length-1)是要pop掉的,所以執行到倒數第二個
       for (let i = 1; i <= this.length - 2; i++) {
         currentNode = currentNode.next;
       }
-      let temp = currentNode.next;
+      let temp = currentNode.next; //倒數第二個的next就是最後一個 ,是要pop掉的
       currentNode.next = null;
       this.length--;
       return temp;
     }
   }
 
+  //pop出第一個值
   shift() {
     if (!this.head) {
       return null;
@@ -61,6 +63,7 @@ class LinkedList {
     }
   }
 
+  //從第一個插入數值
   unshift(value) {
     if (!this.head) {
       this.head = new Node(value);
@@ -73,6 +76,7 @@ class LinkedList {
     this.length++;
   }
 
+  //從特定index插入值(這個index是從 0開始的index)
   insertAt(index, value) {
     if (index > this.length || index < 0) {
       return null;
@@ -86,7 +90,7 @@ class LinkedList {
 
     let currentNode = this.head;
     let newNode = new Node(value);
-    // for loop index - 1 steps
+    // for loop 一直到index的前一個
     for (let i = 1; i <= index - 1; i++) {
       currentNode = currentNode.next;
     }
@@ -96,6 +100,7 @@ class LinkedList {
     return;
   }
 
+  //remove index的值(index是從0開始的)
   removeAt(index) {
     if (index > this.length - 1 || index < 0) {
       return null;
@@ -108,10 +113,12 @@ class LinkedList {
     }
 
     let currentNode = this.head;
+    // for loop 一直到index的前一個
     for (let i = 1; i <= index - 1; i++) {
       currentNode = currentNode.next;
     }
     let temp = currentNode.next;
+    //因為currentNode.next 就是index指定的node, 但因為被指定要被刪除,所以要把index的下一個node接回currentNode.next
     currentNode.next = currentNode.next.next;
     this.length--;
     return temp;
